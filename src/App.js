4b0302b562f8d5da9ca08f99fd59cfa4a874e041
unknown <you@example.c>
2023-02-03 21:19:40 +0600

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,153 @@
-import logo from './logo.svg';
+import Button from '@mui/material/Button';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
+import StopIcon from '@mui/icons-material/Stop';
+import { Paper, Typography } from '@mui/material';
 
 function App() {
+  const [plants, setPlants] = useState(3276882);
+  const [pads, setPads] = useState(327);
+  const [meals, setMeals] = useState(7682);
+  const [projects, setProjects] = useState(276);
+
+  useEffect(() => {
+    setInterval(() => {
+      setPlants((prevPlants) => prevPlants + 10);
+      setPads((prevPads) => prevPads + 10);
+      setMeals((prevMeals) => prevMeals + 10);
+      setProjects((prevProjects) => prevProjects + 10);
+    }, 1000);
+  }, []);
+
+  let id = useRef();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Paper
+        elevation={16}
+        sx={{ height: '50px', mx: 30, backgroundColor: 'green' }}
+      >
+        <Typography variant='h4' color='white' my={2}>
+          Planted Tress
+        </Typography>
+      </Paper>
+      <Paper
+        elevation={16}
+        sx={{
+          width: '400px',
+          height: '80px',
+          mx: 'auto',
+          backgroundColor: 'royalblue',
+        }}
+      >
+        <Typography my={3} variant='h2' color='white'>
+          {plants}{' '}
+        </Typography>
+      </Paper>
+
+      {/* Meal  */}
+
+      <Paper
+        elevation={16}
+        sx={{ height: '50px', mx: 30, backgroundColor: 'green' }}
+      >
+        <Typography variant='h4' color='white' my={2}>
+          Meal Served
+        </Typography>
+      </Paper>
+      <Paper
+        elevation={16}
+        sx={{
+          width: '400px',
+          height: '80px',
+          mx: 'auto',
+          backgroundColor: 'royalblue',
+        }}
+      >
+        <Typography my={3} variant='h2' color='white'>
+          {meals}{' '}
+        </Typography>
+      </Paper>
+
+      {/*-------- Projects  --------*/}
+
+      <Paper
+        elevation={16}
+        sx={{ height: '50px', mx: 30, backgroundColor: 'green' }}
+      >
+        <Typography variant='h4' color='white' my={2}>
+          Projects
+        </Typography>
+      </Paper>
+      <Paper
+        elevation={16}
+        sx={{
+          width: '400px',
+          height: '80px',
+          mx: 'auto',
+          backgroundColor: 'royalblue',
+        }}
+      >
+        <Typography my={3} variant='h2' color='white'>
+          {projects}{' '}
+        </Typography>
+      </Paper>
+
+      {/*-------- Pads Served  --------*/}
+
+      <Paper
+        elevation={16}
+        sx={{ height: '50px', mx: 30, backgroundColor: 'green' }}
+      >
+        <Typography variant='h4' color='white' my={2}>
+          Pads Served
+        </Typography>
+      </Paper>
+      <Paper
+        elevation={16}
+        sx={{
+          width: '400px',
+          height: '80px',
+          mx: 'auto',
+          backgroundColor: 'royalblue',
+        }}
+      >
+        <Typography my={3} variant='h2' color='white'>
+          {pads}{' '}
+        </Typography>
+      </Paper>
+      {/* <Button
+        variant='contained'
+        color='success'
+        sx={{ m: 2 }}
+        onClick={() => handelTime()}
+      >
+        <PlayArrowIcon></PlayArrowIcon>
+        Start
+      </Button>
+      <Button
+        variant='contained'
+        color='warning'
+        sx={{ m: 2 }}
+        onClick={() => clearInterval(id.current)}
+      >
+        <PauseIcon></PauseIcon>
+        Pause
+      </Button>
+      <Button
+        variant='contained'
+        color='error'
+        onClick={() => {
+          clearInterval(id.current);
+          setTime(0);
+        }}
+        sx={{ m: 2 }}
+      >
+        <StopIcon></StopIcon>
+        Reset
+      </Button> */}
     </div>
   );
 }
